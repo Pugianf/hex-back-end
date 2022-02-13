924614aa4148b3f03bb08a4696df14979e10f066
Pugianf <pugianf@gmail.com>
2022-02-12 22:35:32 -0300

    update

---
+++ 0x00-python-network_0/5-post-params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Takes a URL, sends a POST request to the passed URL, and displays the response's body
+curl -sd "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" $1
