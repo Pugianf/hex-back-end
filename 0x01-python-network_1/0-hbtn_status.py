e530dab5826b3b60f351c5c5a8897b50b591a811
Pugianf <pugianf@gmail.com>
2022-02-14 15:35:12 -0300

    update

--- 0x01-python-network_1/0-hbtn_status.py
+++ 0x01-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ import requests
 
 """ Function that makes a request to a url."""
 r = requests.get('https://intranet.hbtn.io/status')
-print("Body response:")
+print('Body response:')
 print("\t- type: {}".format(type(r.text)))
 print("\t- content: {}".format(r.text))
